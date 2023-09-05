ccf15d690d4a9171fb1a3feb833453e5d6887204
hunyoung529 <hunyoung529@gmail.com>
2023-09-05 15:17:08 +0900

    numbering

--- src/App.js
+++ src/App.js
@@ -1,80 +1,75 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-import {useEffect, useState} from 'react';
-import axios from 'axios'
-
-
-function List({data,setData}) {
-
-  const remove = (id)=>{
+function List({ data, setData }) {
+  const remove = (id) => {
     console.log(`${process.env.REACT_APP_SERVER}/abc/${id}`);
 
-    axios.delete(`${process.env.REACT_APP_SERVER}/abc/${id}`)
-    .then(res=>{
-        setData(res.data)
-    })
-  }
+    axios.delete(`${process.env.REACT_APP_SERVER}/abc/${id}`).then((res) => {
+      setData(res.data);
+    });
+  };
 
-  return(
+  return (
     <>
-      {
-        data.map(obj=>(
-          <li key={obj.id}>
-            {obj.msg}
-            <button onClick={()=>{remove(obj.id)}}>삭제</button>
-          </li>
-        ))
-      }
-        
+      {data.map((obj) => (
+        <li key={obj.id}>
+          {obj.msg}
+          <button
+            onClick={() => {
+              remove(obj.id);
+            }}
+          >
+            삭제
+          </button>
+        </li>
+      ))}
     </>
-  )
+  );
 }
 
-function Write({setData}) {
-  const insert = (e)=>{
+function Write({ setData }) {
+  const insert = (e) => {
     e.preventDefault();
 
     let msg = e.target.msg.value;
-    axios.post(`${process.env.REACT_APP_SERVER}/insert`,{msg})
-    .then(res=>{
-      setData(res.data)
-    })
-  }
+    axios
+      .post(`${process.env.REACT_APP_SERVER}/insert`, { msg })
+      .then((res) => {
+        setData(res.data);
+      });
+  };
 
-
-  return(
-      <div>
-        <form onSubmit={insert}>
-          <input type="text" name="msg"/>
-          <input type="submit" value="저장"/>
-        </form>
-      </div>
+  return (
+    <div>
+      <form onSubmit={insert}>
+        <input type="text" name="msg" />
+        <input type="submit" value="저장" />
+      </form>
+    </div>
   );
 }
 
 function App() {
-  const [data,setData]  = useState([]);
+  const [data, setData] = useState([]);
 
-  const getData = ()=>{
-    axios.get(`${process.env.REACT_APP_SERVER}/abc`)
-    .then(res=>{
+  const getData = () => {
+    axios.get(`${process.env.REACT_APP_SERVER}/abc`).then((res) => {
       setData(res.data);
     });
-  }
-  
-  useEffect(() => {
-    getData();  
-  }, []);
-  
-  
+  };
 
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <div>
       <h2>한줄댓글{data.length}</h2>
-      <Write setData={setData}/>
+      <Write setData={setData} />
 
       <ul>
-        <List data={data} setData={setData}/>
+        <List data={data} setData={setData} />
       </ul>
     </div>
   );
